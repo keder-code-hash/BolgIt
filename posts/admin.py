de6a7d7548d98f6ce57900d17c239d8674156d8d
Keder <kedernath.mallick.tint022@gmail.com>
2021-10-16 16:40:49 +0530

    admin_pannel_started

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from posts.models import Posts
+from posts.models import Comments, Posts, postTag
 # from django.contrib.admin
 
 
 admin.site.register(Posts)
+admin.site.register(Comments)
+admin.site.register(postTag)
 # Register your models here.
